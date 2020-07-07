59fbc10268188579e65a9fe17b5c439b874506fb
moon <62694941+eom16@users.noreply.github.com>
2020-07-07 11:05:17 +0900

    Update EventboardDao.java

--- gamedori/src/gamedori/beans/dao/EventboardDao.java
+++ gamedori/src/gamedori/beans/dao/EventboardDao.java
@@ -38,9 +38,7 @@ public class EventboardDao {
 	
 	//연결 메소드
 	public Connection getConnection() throws Exception {
-//		Class.forName("oracle.jdbc.OracleDriver");
-//		return DriverManager.getConnection(
-//				"jdbc:oracle:thin:@localhost:1521:xe", "c##kh", "c##kh");
+
 		return src.getConnection();
 	}
 	
