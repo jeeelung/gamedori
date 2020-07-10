04867b824f274cf3ac8a213eb3f554fd5d82b381
heeky77777 <heeky77777@naver.com>
2020-07-10 18:09:23 +0900

    업로드

--- gamedori/src/gamedori/servlet/notice/NoticeWriteServlet.java
+++ gamedori/src/gamedori/servlet/notice/NoticeWriteServlet.java
@@ -1,6 +1,9 @@
 package gamedori.servlet.notice;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,11 +11,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
+
 import gamedori.beans.dao.NoticeDao;
 import gamedori.beans.dto.MemberDto;
 import gamedori.beans.dto.NoticeDto;
+import gamedori.beans.dto.NoticeFileDto;
 
-@WebServlet(urlPatterns = "/gamedori/write.do")
+@WebServlet(urlPatterns = "/notice/write.do")
 public class NoticeWriteServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,24 +29,66 @@ public class NoticeWriteServlet extends HttpServlet {
 			// 목표 : 게시글 등록 후 상세 페이지로 이동
 			// 준비 : 제목, 내용, 작성자
 
-			MemberDto mdto = (MemberDto) req.getSession().getAttribute("userinfo");
-			int member_no = mdto.getMember_no(); // 작성자 추출
-
+			String charset = "UTF-8"; // 해석할 인코딩 방식
+			int limit = 10 * 1024 * 1024; // 최대 허용 용량
+			File baseDir = new File("D:/upload/community");
+			baseDir.mkdirs();
+			
+			DiskFileItemFactory factory = new DiskFileItemFactory(limit, baseDir);
+			factory.setDefaultCharset(charset);
+			
+			ServletFileUpload utility = new ServletFileUpload(factory);
+			
+			Map<String, List<FileItem>> map = utility.parseParameterMap(req);
+			
 			NoticeDto ndto = new NoticeDto();
-			ndto.setNotice_title(req.getParameter("notice_title"));
-			ndto.setNotice_content(req.getParameter("notice_content"));
-			ndto.setMember_no(Integer.parseInt("member_no"));
-
+			ndto.setNotice_no(Integer.parseInt(map.get("notice_no").get(0).getString()));
+			ndto.setMember_no(Integer.parseInt(map.get("member_no").get(0).getString()));
+			ndto.setNotice_title(map.get("notice_title").get(0).getString());
+			ndto.setNotice_content(map.get("notice_content").get(0).getString());
+			
+						
 			NoticeDao ndao = new NoticeDao();
-			int notice_no = ndao.getSequence(); // 번호 먼저 추출
-			ndto.setNotice_no(notice_no); // 번호를 설정한 뒤
-			ndao.write(ndto); // 등록
-
-			resp.sendRedirect("content.jsp?notice_no=" + notice_no);
-     		} 
-			catch (Exception e) {
+			int notice_no = ndao.getSequence();
+			ndto.setNotice_no(notice_no);
+			
+			ndao.write(ndto);
+			
+			List<FileItem> fileList = map.get("notice_file");
+			
+			for(FileItem item : fileList) {
+				
+				if(item.getSize() > 0) { // 파일이 있는 경우
+					
+					FilesDao fdao = new FilesDao();
+					FilesDto fdto = new FilesDto();
+					
+					int file_no = fdao.getSequence();
+					fdto.setFile_no(file_no);
+					fdto.setFile_name(item.getName());
+					fdto.setFile_size(item.getSize());
+					fdto.setFile_type(item.getContentType());
+					fdao.save(fdto);
+					
+					NoticeFileDto nfdto = new NoticeFileDto();
+					nfdto.setNotice_no(notice_no);
+					nfdto.setFile_no(file_no);
+					
+	//				NoticeFileDao nfdao = new NoticeFileDao();
+	//				nfdao.save(nfdto);
+					
+					File target = new File(baseDir, String.valueOf(file_no));
+					item.write(target);
+				}
+			}
+			
+			resp.sendRedirect("content.jsp?notice_no="+notice_no);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 			resp.sendError(500);
 		}
+		
 	}
 }
